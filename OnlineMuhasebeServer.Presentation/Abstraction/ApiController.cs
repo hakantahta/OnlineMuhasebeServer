152e528ad05fc8632b02e8e88859c923714583bc
tahta <tahtahakan54@gmail.com>
2025-05-15 17:19:02 +0300

    Add project files.

---
+++ OnlineMuhasebeServer.Presentation/Abstraction/ApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineMuhasebeServer.Presentation.Abstraction
+{
+    [ApiController]
+    [Route("Api/[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+
+    }
+}
---
+++ OnlineMuhasebeServer.Presentation/AssemblyReference.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+//web api deki controller ı presentation katmanında kullanmamız lazım çünkü presentationta kullanmazsak diğer tüm katmanlar 
+//web api ye bağlı olmak ve referans almak zorunda kalavak biz bunu istemiyoruz gevşek bağımlılık istiyoruz ne kadar az bağımlılık o kadariyi.
+//controller ı prsentationda kullanabilmemiz için onun assembly sini bu katmana oluşturup program.cs ye vermememiz gerekiyor. 
+
+namespace OnlineMuhasebeServer.Presentation
+{
+    public  static class AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(Assembly).Assembly;
+    }
+}
