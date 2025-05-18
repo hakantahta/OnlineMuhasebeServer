594c83cab53a8ec04fd870a0e108afa18f32fca9
tahta <tahtahakan54@gmail.com>
2025-05-18 18:48:29 +0300

    Set up the Repository Pattern structure

---
+++ OnlineMuhasebeServer.Domain/Repositories/ICommandRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineMuhasebeServer.Domain.Abstractions;
+
+namespace OnlineMuhasebeServer.Domain.Repositories
+{
+    public interface ICommandRepository<T> : IRepository<T>
+        where T : Entity
+    {
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        Task RemoveById(String id);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+    }
+}
