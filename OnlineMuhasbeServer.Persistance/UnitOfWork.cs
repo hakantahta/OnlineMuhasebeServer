05e9ed36aca658673749ae27d97ba82362f812c9
tahta <tahtahakan54@gmail.com>
2025-05-25 21:20:04 +0300

    Write transactions related to UCAF

---
+++ OnlineMuhasbeServer.Persistance/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineMuhasbeServer.Persistance.Context;
+using OnlineMuhasebeServer.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMuhasebeServer.Persistance
+{
+    public sealed class UnitOfWork : IUnitOfWork
+    {
+        private CompanyDbContext _context;
+        public void CreateDbContextInstance(DbContext context)
+        {
+            _context = (CompanyDbContext)context;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            int count = await _context.SaveChangesAsync();
+            return count;
+        }
+    }
+}
--- OnlineMuhasebeServer.WebApi/Program.cs
+++ OnlineMuhasebeServer.WebApi/Program.cs
@@ -2,7 +2,12 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using OnlineMuhasbeServer.Persistance.Context;
 using OnlineMuhasebeServer.Application.Services.AppServices;
+using OnlineMuhasebeServer.Domain;
 using OnlineMuhasebeServer.Domain.AppEntities.Identity;
+using OnlineMuhasebeServer.Domain.Repositories.UCAFRepositories;
+using OnlineMuhasebeServer.Persistance;
+using OnlineMuhasebeServer.Persistance.Repositories;
+using OnlineMuhasebeServer.Persistance.Repositories.UCAFRepositories;
 using OnlineMuhasebeServer.Persistance.Services.AppServices;
 using OnlineMuhasebeServer.Presentation;
 
@@ -16,6 +21,10 @@ builder.Services.AddIdentity<AppUser,AppRole>().
     AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddScoped<ICompanyService, CompanyService>();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IUCAFCommandRepository , UCAFCommandRepository>();
+builder.Services.AddScoped<IUCAFQueryRepository , UCAFQueryRepository>();
 
 
 builder.Services.AddMediatR(typeof(OnlineMuhasebeServer.Application.AssemblyReference).Assembly);
