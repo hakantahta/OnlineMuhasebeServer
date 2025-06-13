5e6f78b1de3f99f6a3e6f56d92f1a7ec6593f9ef
tahta <tahtahakan54@gmail.com>
2025-06-13 20:11:38 +0300

    Create Json Web Token structure and generate Token, use Options Pattern for this structure

---
+++ OnlineMuhasebeServer.Application/Abstractions/IJwtProwider.cs
@@ -0,0 +1,11 @@
+﻿using OnlineMuhasebeServer.Domain.AppEntities.Identity;
+
+using OnlineMuhasebeServer.Domain.AppEntities.Identity;
+
+namespace OnlineMuhasebeServer.Application.Abstractions
+{
+    public interface IJwtProvider
+    {
+        Task<string> CreateTokenAsync(AppUser user, List<string> roles);
+    }
+}
--- OnlineMuhasebeServer.WebApi/Program.cs
+++ OnlineMuhasebeServer.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Identity;
+using OnlineMuhasebeServer.Domain.AppEntities.Identity;
 using OnlineMuhasebeServer.WebApi.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,5 +23,23 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scoped = app.Services.CreateScope())
+{
+    var userManager = scoped.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+if (!userManager.Users.Any())
+{
+        userManager.CreateAsync(new AppUser
+        {
+            UserName = "htahta",
+            Email = "tahtahakan54@gmail.com",
+            Id = Guid.NewGuid().ToString(),
+            NameLastName = "Hakan Tahta",
+            RefreshToken = Guid.NewGuid().ToString(), // veya uygun bir random deðer
+            RefreshTokenExpires = DateTime.UtcNow.AddDays(30)
+        }, "Password12*").Wait();
+
+    }
+}
 
 app.Run();
